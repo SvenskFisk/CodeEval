9227b2683e92835f462b6150b4cc91e62ea15c5d
Jonas Jämtberg <jonas@jamtberg.se>
2015-09-21 20:27:02 +0200

    Solved

---
+++ 214-Time_to_Eat/ClassLibrary1/Solver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Solver
+    {
+        public string SortDates(string datesString)
+        {
+            var sb = new StringBuilder(datesString.Length);
+            var first = true;
+
+            foreach (var s in datesString.Split(' ')
+                .OrderByDescending(x => x))
+            {
+                if (!first)
+                {
+                    sb.Append(' ');
+                }
+
+                sb.Append(s);
+                first = false;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 214-Time_to_Eat/ClassLibrary1/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("02:26:31 14:44:45 09:53:27", "14:44:45 09:53:27 02:26:31")]
+        [InlineData("05:33:44 21:25:41", "21:25:41 05:33:44")]
+        public void SortDates(string input, string expected)
+        {
+            var t = new Solver();
+            var result = t.SortDates(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
